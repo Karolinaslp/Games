e80b44a43569cb4da14dfbe81534e5a1c6fc7c45
Karolinaslp <slempkarolina@gmail.com>
2022-02-06 22:40:54 +0100

    Initial commit - frame for Snake Game

---
+++ src/main/java/snake/SnakeGame.java
@@ -0,0 +1,29 @@
+package snake;
+
+
+import snake.codegym_classes.Color;
+import snake.codegym_classes.Game;
+
+public class SnakeGame extends Game {
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+
+    @Override
+    public void initialize() {
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+
+
+    private void createGame() {
+        drawScene();
+    }
+
+    private void drawScene() {
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIDTH; x++) {
+                setCellColor(x, y, Color.DARKSEAGREEN);
+            }
+        }
+    }
+}
---
+++ src/main/java/snake/codegym_classes/Key.java
@@ -0,0 +1,16 @@
+package snake.codegym_classes;
+
+public enum Key {
+    UNKNOWN,
+    ENTER,
+    ESCAPE,
+    SPACE,
+    PAUSE,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN;
+
+    Key() {
+    }
+}
